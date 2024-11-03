d35e87f662cb033a865fd71e4be76baf76039971
Carl Lindberg <carlzxc71@gmail.com>
2024-11-03 08:50:38 +0100

    Initial commit

---
+++ main.tf
@@ -0,0 +1,59 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.8.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+data "azurerm_client_config" "current" {}
+
+data "azurerm_subscription" "primary" {}
+
+resource "azurerm_resource_group" "this" {
+  name     = "rg-${var.environment}-${var.location_short}-pim"
+  location = var.location
+}
+
+data "azurerm_role_definition" "reader" {
+  name = "Reader"
+}
+
+data "azurerm_role_definition" "contributor" {
+  name = "contributor"
+}
+
+resource "azurerm_role_assignment" "reader" {
+  scope                = azurerm_resource_group.this.id
+  role_definition_name = "Reader"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_role_management_policy" "contributor" {
+  scope              = azurerm_resource_group.this.id
+  role_definition_id = data.azurerm_role_definition.contributor.role_definition_id
+
+  eligible_assignment_rules {
+    expiration_required = false
+  }
+
+  activation_rules {
+    maximum_duration      = "PT8H"
+    require_approval      = false
+    require_justification = true
+  }
+}
+
+resource "time_static" "this" {}
+
+resource "azurerm_pim_eligible_role_assignment" "contributor" {
+  scope              = azurerm_resource_group.this.id
+  role_definition_id = "${data.azurerm_subscription.primary.id}${data.azurerm_role_definition.contributor.id}"
+  principal_id       = data.azurerm_client_config.current.object_id
+}
